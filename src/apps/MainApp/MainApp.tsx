9a2fe2b7fa4bf4f269b77e227d3438f61890b1ce
vitalyvitmens <otm_@tut.by>
2024-03-19 16:52:03 +0300

    chore(src\apps\MainApp\MainApp.tsx): подключает redux

--- src/apps/MainApp/MainApp.tsx
+++ src/apps/MainApp/MainApp.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './MainApp.scss'
 import { ThemeProvider } from 'react-bootstrap'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -10,96 +9,36 @@ import {
   FavoritListPage,
   GroupListPage,
 } from 'src/pages'
-import { ContactDto } from 'src/types/dto/ContactDto'
-import { FavoriteContactsDto } from 'src/types/dto/FavoriteContactsDto'
-import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
-import { DATA_CONTACT, DATA_GROUP_CONTACT } from 'src/__data__'
+import { persistor, store } from 'src/redux/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export const MainApp = () => {
-  const contactsState = useState<ContactDto[]>(DATA_CONTACT)
-  const favoriteContactsState = useState<FavoriteContactsDto>([
-    DATA_CONTACT[0].id,
-    DATA_CONTACT[1].id,
-    DATA_CONTACT[2].id,
-    DATA_CONTACT[3].id,
-  ])
-  const groupContactsState = useState<GroupContactsDto[]>(DATA_GROUP_CONTACT)
-
   return (
-    <ThemeProvider
-      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-      minBreakpoint="xxs"
-    >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route
-              index
-              element={
-                <ContactListPage
-                  contactsState={contactsState}
-                  favoriteContactsState={favoriteContactsState}
-                  groupContactsState={groupContactsState}
-                />
-              }
-            />
-            <Route path="contact">
-              <Route
-                index
-                element={
-                  <ContactListPage
-                    contactsState={contactsState}
-                    favoriteContactsState={favoriteContactsState}
-                    groupContactsState={groupContactsState}
-                  />
-                }
-              />
-              <Route
-                path=":contactId"
-                element={
-                  <ContactPage
-                    contactsState={contactsState}
-                    favoriteContactsState={favoriteContactsState}
-                    groupContactsState={groupContactsState}
-                  />
-                }
-              />
-            </Route>
-            <Route path="groups">
-              <Route
-                index
-                element={
-                  <GroupListPage
-                    contactsState={contactsState}
-                    favoriteContactsState={favoriteContactsState}
-                    groupContactsState={groupContactsState}
-                  />
-                }
-              />
-              <Route
-                path=":groupId"
-                element={
-                  <GroupPage
-                    contactsState={contactsState}
-                    favoriteContactsState={favoriteContactsState}
-                    groupContactsState={groupContactsState}
-                  />
-                }
-              />
-            </Route>
-            <Route
-              path="favorit"
-              element={
-                <FavoritListPage
-                  contactsState={contactsState}
-                  favoriteContactsState={favoriteContactsState}
-                  groupContactsState={groupContactsState}
-                />
-              }
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider
+          breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+          minBreakpoint="xxs"
+        >
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<ContactListPage />} />
+                <Route path="contact">
+                  <Route index element={<ContactListPage />} />
+                  <Route path=":contactId" element={<ContactPage />} />
+                </Route>
+                <Route path="groups">
+                  <Route index element={<GroupListPage />} />
+                  <Route path=":groupId" element={<GroupPage />} />
+                </Route>
+                <Route path="favorit" element={<FavoritListPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   )
 }
