7fa9caa17155f9c6f7abbc979b4067a0f07b3d23
vitalyvitmens <otm_@tut.by>
2024-03-20 14:13:04 +0300

    delete: удаляет файлы

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { ContactListPage } from './ContactListPage'
-export { FavoritListPage } from './FavoritListPage'
-export { GroupListPage } from './GroupListPage'
-export { GroupPage } from './GroupPage'
-export { ContactPage } from './ContactPage'
