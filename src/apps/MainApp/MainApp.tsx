551c5bd11101f18a5def40cd4d661938f8f65b70
vitalyvitmens <otm_@tut.by>
2024-03-20 14:15:39 +0300

    chore(src\apps\MainApp\MainApp.tsx): изменяет отображение роутов

--- src/apps/MainApp/MainApp.tsx
+++ src/apps/MainApp/MainApp.tsx
@@ -1,17 +1,13 @@
-import './MainApp.scss'
-import { ThemeProvider } from 'react-bootstrap'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Layout } from 'src/components/Layout'
-import {
-  ContactListPage,
-  GroupPage,
-  ContactPage,
-  FavoritListPage,
-  GroupListPage,
-} from 'src/pages'
-import { persistor, store } from 'src/redux/store'
+import { Suspense } from 'react'
+import { routes } from '../../routes'
+import { BrowserRouter, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { persistor, store } from 'src/redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import ErrorBoundary from 'src/components/ErrorBoundary/ErrorBoundary'
+import { renderRoutes } from 'src/utils/renderRoutes'
+import { ThemeProvider } from 'react-bootstrap'
+import './MainApp.scss'
 
 export const MainApp = () => {
   return (
@@ -22,20 +18,11 @@ export const MainApp = () => {
           minBreakpoint="xxs"
         >
           <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<ContactListPage />} />
-                <Route path="contact">
-                  <Route index element={<ContactListPage />} />
-                  <Route path=":contactId" element={<ContactPage />} />
-                </Route>
-                <Route path="groups">
-                  <Route index element={<GroupListPage />} />
-                  <Route path=":groupId" element={<GroupPage />} />
-                </Route>
-                <Route path="favorit" element={<FavoritListPage />} />
-              </Route>
-            </Routes>
+            <ErrorBoundary>
+              <Suspense>
+                <Routes>{renderRoutes(routes)}</Routes>
+              </Suspense>
+            </ErrorBoundary>
           </BrowserRouter>
         </ThemeProvider>
       </PersistGate>
