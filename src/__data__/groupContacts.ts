a616e32cb6d9da0b7c41a0fb2fc3355ed73a9f5e
vitalyvitmens <otm_@tut.by>
2024-04-02 17:09:06 +0300

    build: билдит проект для деплой

--- src/__data__/groupContacts.ts
+++ src/__data__/groupContacts.ts
@@ -5,7 +5,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "fcc6cb46-1fbd-4a4b-bad6-b134a1eb2f50",
     "name": "sed-nostrum-nisi",
     "description": "Sint fugit nostrum animi. Laudantium voluptatem et eos quam delectus in. Sed omnis et praesentium blanditiis.",
-    "photo": "../assets/images/id-164-200-300.jpg",
+    "photo": "/images/id-164-200-300.jpg",
     "contactIds": [
       "ecd667da-0513-4dd5-ba50-e7cc69f6573c",
       "84465d40-ef7b-41c7-8de4-29e7fb4ddd21",
@@ -29,7 +29,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "73fd039a-0791-48d8-9b69-7c43985c6ed4",
     "name": "dolorum-repellendus-ex",
     "description": "Et quibusdam quos iusto. Non sunt perferendis error nisi sint qui nam.",
-    "photo": "../assets/images/id-837-200-300.jpg",
+    "photo": "/images/id-837-200-300.jpg",
     "contactIds": [
       "84465d40-ef7b-41c7-8de4-29e7fb4ddd21",
       "0fc3c0ea-0e30-439b-bf2d-393e044788b2",
@@ -52,7 +52,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "45915d97-3525-4901-9cb1-eb932d9075b5",
     "name": "alias-velit-quae",
     "description": "Rerum veritatis assumenda odio laborum sequi quam. Distinctio et autem inventore molestiae quia. Sunt amet quam voluptatem. Delectus aut adipisci.",
-    "photo": "../assets/images/id-757-200-300.jpg",
+    "photo": "/images/id-757-200-300.jpg",
     "contactIds": [
       "f70f71ac-937a-44a4-b7cc-654ba4a1b915",
       "6d71425d-fd8c-4f6e-a676-f4191ec5e901",
@@ -68,7 +68,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "6f91a812-26bc-418f-9841-ac56883303f2",
     "name": "sed-nostrum-nulla",
     "description": "Ullam molestiae asperiores.",
-    "photo": "../assets/images/id-787-200-300.jpg",
+    "photo": "/images/id-787-200-300.jpg",
     "contactIds": [
       "9dd3369c-e35e-4cff-922c-a3dd16cc475a"
     ]
@@ -77,7 +77,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "2a71c485-ac85-4e6f-a024-7381333f03b2",
     "name": "exercitationem-et-facilis",
     "description": "Ipsum dolore eum corrupti nobis placeat ipsa.",
-    "photo": "../assets/images/id-378-200-300.jpg",
+    "photo": "/images/id-378-200-300.jpg",
     "contactIds": [
       "f70f71ac-937a-44a4-b7cc-654ba4a1b915",
       "6d71425d-fd8c-4f6e-a676-f4191ec5e901",
@@ -88,7 +88,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "fb76d02e-1bee-4f8e-8a11-e43c0b2b78a6",
     "name": "esse-qui-ut",
     "description": "Commodi iste id et suscipit minima laborum.",
-    "photo": "../assets/images/id-116-200-300.jpg",
+    "photo": "/images/id-116-200-300.jpg",
     "contactIds": [
       "d97eb312-c502-48aa-b8d6-a427b8950ce1"
     ]
@@ -97,7 +97,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "eae7a5bf-d25c-4b50-bbb3-61aae0852c9f",
     "name": "neque-odit-porro",
     "description": "Temporibus repellendus et voluptas et aperiam. Necessitatibus officiis fugit est. Ut voluptatem et voluptatibus at.",
-    "photo": "../assets/images/id-718-200-300.jpg",
+    "photo": "/images/id-718-200-300.jpg",
     "contactIds": [
       "84465d40-ef7b-41c7-8de4-29e7fb4ddd21",
       "0fc3c0ea-0e30-439b-bf2d-393e044788b2",
@@ -120,7 +120,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "ae69a945-edaa-4297-9778-b7ef533c61c3",
     "name": "minus-ipsam-officiis",
     "description": "Nemo praesentium corrupti quisquam repellat quas dolor consequatur. Quam dolorem laborum laudantium. Fugit possimus blanditiis.",
-    "photo": "../assets/images/id-622-200-300.jpg",
+    "photo": "/images/id-622-200-300.jpg",
     "contactIds": [
       "08b8735f-c1b6-4029-a96b-3d3e91869727",
       "9dd3369c-e35e-4cff-922c-a3dd16cc475a",
@@ -141,7 +141,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "9fbb7428-5a6d-4a9c-86e1-d93b5fc55460",
     "name": "quibusdam-animi-velit",
     "description": "Et porro aut. Porro laboriosam autem in consequatur quia.",
-    "photo": "../assets/images/id-620-200-300.jpg",
+    "photo": "/images/id-620-200-300.jpg",
     "contactIds": [
       "6d71425d-fd8c-4f6e-a676-f4191ec5e901",
       "9030a77e-7e6c-4052-9736-438b9252073e",
@@ -153,7 +153,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "31f5ea41-a21f-4349-9952-cc0bb4a83eb6",
     "name": "labore-numquam-iure",
     "description": "Beatae error quo dolorum dolor possimus odit soluta facere facilis. Aut unde veritatis et assumenda ea vero excepturi magni.",
-    "photo": "../assets/images/id-3-200-300.jpg",
+    "photo": "/images/id-3-200-300.jpg",
     "contactIds": [
       "84465d40-ef7b-41c7-8de4-29e7fb4ddd21",
       "0fc3c0ea-0e30-439b-bf2d-393e044788b2",
@@ -176,7 +176,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "eca43af2-e25e-414c-98ae-bfa6e8eb447c",
     "name": "quisquam-nihil-accusantium",
     "description": "Iste voluptas et corporis error in mollitia et. Labore hic iure sed sequi reprehenderit ut possimus. Voluptas ut aut quo placeat porro impedit ut labore voluptatem.",
-    "photo": "../assets/images/id-72-200-300.jpg",
+    "photo": "/images/id-72-200-300.jpg",
     "contactIds": [
       "08b8735f-c1b6-4029-a96b-3d3e91869727",
       "9dd3369c-e35e-4cff-922c-a3dd16cc475a",
@@ -187,7 +187,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "a7222b4e-6bba-41c2-b613-0b75420029ec",
     "name": "voluptas-provident-at",
     "description": "Sint voluptas recusandae temporibus accusamus voluptatem voluptas. Esse voluptates aut quisquam ad nam.",
-    "photo": "../assets/images/id-485-200-300.jpg",
+    "photo": "/images/id-485-200-300.jpg",
     "contactIds": [
       "08b8735f-c1b6-4029-a96b-3d3e91869727",
       "9dd3369c-e35e-4cff-922c-a3dd16cc475a",
@@ -207,7 +207,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "fe29fbca-ad4b-40fe-98e6-189441bdbe84",
     "name": "omnis-tempora-et",
     "description": "Quod voluptates tempora soluta enim nihil tempore aliquam sequi mollitia. Natus est exercitationem velit doloribus nihil.",
-    "photo": "../assets/images/id-795-200-300.jpg",
+    "photo": "/images/id-795-200-300.jpg",
     "contactIds": [
       "08b8735f-c1b6-4029-a96b-3d3e91869727",
       "9dd3369c-e35e-4cff-922c-a3dd16cc475a",
@@ -221,7 +221,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "b2d4939c-8db2-480d-9136-1ae639c7b406",
     "name": "qui-totam-quod",
     "description": "Neque a quo dolore saepe. Odio nemo porro dolor.",
-    "photo": "../assets/images/id-802-200-300.jpg",
+    "photo": "/images/id-802-200-300.jpg",
     "contactIds": [
       "84465d40-ef7b-41c7-8de4-29e7fb4ddd21",
       "0fc3c0ea-0e30-439b-bf2d-393e044788b2",
@@ -243,7 +243,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "12b79151-3c27-45b1-b912-c4ac48d09b06",
     "name": "quaerat-illum-cumque",
     "description": "Fuga quia explicabo. Consequuntur occaecati rerum eos et eligendi eos libero.",
-    "photo": "../assets/images/id-877-200-300.jpg",
+    "photo": "/images/id-877-200-300.jpg",
     "contactIds": [
       "84465d40-ef7b-41c7-8de4-29e7fb4ddd21",
       "0fc3c0ea-0e30-439b-bf2d-393e044788b2",
@@ -265,7 +265,7 @@ export const groupContacts: GroupContactsDto[] = [
     "id": "89e8ff55-3783-457e-adee-15968413b15c",
     "name": "consectetur-non-magni",
     "description": "Beatae ut ducimus suscipit fuga aut cumque molestias cum qui. Est dignissimos quibusdam quibusdam. Cumque rerum incidunt.",
-    "photo": "../assets/images/id-845-200-300.jpg",
+    "photo": "/images/id-845-200-300.jpg",
     "contactIds": [
       "0fc3c0ea-0e30-439b-bf2d-393e044788b2",
       "08b8735f-c1b6-4029-a96b-3d3e91869727",
